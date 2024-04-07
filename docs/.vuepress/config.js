c284f2e8f1740beae29fcf35f973d0684bf09b40
wwangyu2 <129188737+wwangyu2@users.noreply.github.com>
2024-04-07 09:21:07 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import fullTextSearchPlugin from "vuepress-plugin-full-text-search2";
 
 export default defineUserConfig({
-  base:'/blog-web/',//部署站点的基础路径,默认/
+  base:'/',//部署站点的基础路径,默认/
   lang: 'zh-CN',
 
   title: '不知道文档站',
